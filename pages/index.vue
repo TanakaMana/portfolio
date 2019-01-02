6ab365c512cb1c52a9ffda9bf33380f90d571f07
TanakaMana <makkuromyaon@gmail.com>
2019-01-02 14:03:41 +0900

    initialize commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div
+        v-for="(feature, i) of features"
+        :key="i"
+        class="column">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title has-text-grey">
+              {{ feature.title }}
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content has-text-centered">
+              <b-icon
+                :icon="feature.icon"
+                size="is-large"
+                type="is-primary"/>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <div
+              class="card-footer-item"
+              v-html="feature.content"/>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  name: 'HomePage',
+  components: { Logo }
+  // data() {
+  //     return {
+  //     }
+  // }
+}
+</script>
